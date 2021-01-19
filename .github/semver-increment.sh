2f172518309af8c72868ae8566ac8d3dac42e50d
Dimitris Zervas <dzervas@noc.grnet.gr>
2021-01-19 13:07:15 +0200

    Add version bumper

---
+++ .github/semver-increment.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if [ "$#" -lt 1  ]; then
+	echo "Usage: $0 <old_version> [new_version|increment_strategy]"
+	exit 1
+fi
+
+export OLD_VERSION=$1
+export MAJOR=$(echo -n "$OLD_VERSION" | sed -E "s/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/")
+export MINOR=$(echo -n "$OLD_VERSION" | sed -E "s/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/")
+export PATCH=$(echo -n "$OLD_VERSION" | sed -E "s/([0-9]+)\.([0-9]+)\.([0-9]+)/\3/")
+
+if [[ "$2" == "major" ]]; then
+	MAJOR=$(($MAJOR + 1))
+elif [[ "$2" == "minor" ]]; then
+	MINOR=$(($MINOR + 1))
+elif [[ "$2" == "patch" ]]; then
+	PATCH=$(($PATCH + 1))
+elif [ "$#" -lt 2  ]; then
+	# No strategy/new version was given. Increment patch
+	PATCH=$(($PATCH + 1))
+else
+	# Custom version, print it and exit
+	echo -n "$2"
+	exit 0
+fi
+
+echo -n "$MAJOR.$MINOR.$PATCH"
